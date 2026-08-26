289df4d5318d3adad953357bcabf452ad9d4fddc
Nikhil Kumar Singh <nkumarsingh575@gmail.com>
2026-08-27 01:09:53 +0530

    Containerize Flask application

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/health")
 def health_check():
-    return {
+    return jsonify({
         "status": "healthy",
         "service": "devops-learning"
-    }
+    })
 
 
 if __name__ == "__main__":
-    print(health_check())
+    app.run(host="0.0.0.0", port=5000)
